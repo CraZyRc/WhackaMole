5bca30c02b92c0be770a9f85e12a79b81407d062
GnarLito <gnarlito35@gmail.com>
2023-08-01 21:39:59 +0200

    Fixed overlapping game grids

--- src/main/java/whackamole/whackamole/DB/GridDB.java
+++ src/main/java/whackamole/whackamole/DB/GridDB.java
@@ -13,6 +13,7 @@ public class GridDB extends Table<GridRow> {
 
     public GridDB(SQLite sql) {
         super(sql, "Grid", new Column<?>[] {
+            new Column<>("worldName", String.class).AllowNull(false),
             new Column<>("gameID", Integer.class).AllowNull(false),
             new Column<>("X", Integer.class).AllowNull(false),
             new Column<>("Y", Integer.class).AllowNull(false),
@@ -24,28 +25,34 @@ public class GridDB extends Table<GridRow> {
     public List<GridRow> Select(int gameID) {
         return this.Select("gameID = ?", gameID);
     }
+    
+    public List<GridRow> Select(String worldName) {
+        return this.Select("worldName = ?", worldName);
+    }
 
     public List<GridRow> Insert(Grid grid, int gameID) {
+        String worldName = grid.world.getName();
         List<GridRow> rowList = new ArrayList<>();
         for (Block block : grid.grid) {
-            rowList.add(this.Insert(block, gameID));
+            rowList.add(this.Insert(block, worldName, gameID));
         }
         return rowList;
     }
 
-    public GridRow Insert(Block block, int gameID) {
+    public GridRow Insert(Block block, String worldName, int gameID) {
         var row = new GridRow();
         row.gameID = gameID;
         row.X = block.getX();
         row.Y = block.getY();
         row.Z = block.getZ();
+        row.worldName = worldName;
         return this.Insert(row);
     }
 
     public void Delete(int gameID) {
-        this.Delete("GameID = ?", gameID);
+        this.Delete("gameID = ?", gameID);
     }
     public void Delete(GridRow row) {
-        this.Delete("GameID = ? AND X = ? AND Y = ? AND Z = ?", row.gameID, row.X, row.Y, row.Z);
+        this.Delete("worldName = ? AND gameID = ? AND X = ? AND Y = ? AND Z = ?", row.worldName, row.gameID, row.X, row.Y, row.Z);
     }
 }
